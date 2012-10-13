ae8fb0b464075f48965d50c92610ab77fa3b24a0
Balazs Ree <ree@greenfinity.hu>
2012-10-13 18:45:55 +0200

    Progress with our first functions

--- test/urlconvert-test.js
+++ test/urlconvert-test.js
@@ -2,18 +2,45 @@ var buster = require("buster");
 var assert = buster.assert;
 var M = require("../lib/urlconvert.js");
 
-
 buster.testCase("buster-static", {
 
     "encodeURL": {
 
         "converts the spaces": function () {
             assert.equals(
-                M.encodeURL('something wonderful'),
-                'something_wonderful'
+                M.encodeURL('Something wonderful.'),
+                'Something-wonderful.'
             );
+        },
+
+        "converts the dashes": function () {
+            assert.equals(
+                M.encodeURL('Something-wonderful.'),
+                'Something%2dwonderful.'
+            );
+ 
+        }
+
+    },
+
+    "decodeURL": {
+
+        "converts the spaces": function () {
+            assert.equals(
+                M.decodeURL('Something-wonderful.'),
+                'Something wonderful.'
+            );
+        },
+
+        "converts the dashes": function () {
+            assert.equals(
+                M.decodeURL('Something%2dwonderful.'),
+                'Something-wonderful.'
+            );
+ 
         }
 
     }
+
 
 });
