18456932ef64ea6ba01efa92a8d5831b0d4c3100
Balazs Ree <ree@greenfinity.hu>
2012-10-13 18:05:02 +0200

    add something

---
+++ test/urlconvert-test.js
@@ -0,0 +1,12 @@
+var buster = require("buster");
+var assert = buster.assert;
+var M = require("../lib/urlconvert.js");
+
+
+buster.testCase("buster-static", {
+
+    "do something already": function () {
+        assert(true);
+    }
+
+});
